1c177dbb84a8d9bd9ceaf9b42ae893570a11987b
Mikhail Bagmet <m.bagmet@gmail.com>
2021-12-28 00:55:58 +0200

    Add TabBarController with views and navigation

--- IOS4_HW13_MIKHAIL_BAGMET/Sources/App/AppDelegate.swift
+++ IOS4_HW13_MIKHAIL_BAGMET/Sources/App/AppDelegate.swift
@@ -10,27 +10,56 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let tabBarController = UITabBarController()
+
+        // MARK: - "Media Libruary" tab
+        let mediaLibruaryViewController = MediaLibruaryViewController()
+        let mediaLibruaryNavigationController = UINavigationController(rootViewController: mediaLibruaryViewController)
+        mediaLibruaryViewController.tabBarItem = UITabBarItem(title: MediaLibruaryViewController.Strings.navigationTitle,
+                                                              image: UIImage(systemName: "photo.fill.on.rectangle.fill"),
+                                                              tag: 0)
+
+        // MARK: - "For You" tab
+        let forYouViewController = ForYouViewController()
+        let forYouNavigationController = UINavigationController(rootViewController: forYouViewController)
+        forYouViewController.tabBarItem = UITabBarItem(title: ForYouViewController.Strings.navigationTitle,
+                                                       image: UIImage(systemName: "heart.text.square.fill"),
+                                                       tag: 1)
+
+        // MARK: - "Albums" tab
+        let albumsViewController = UIViewController()
+        let albumsNavigationController = UINavigationController(rootViewController: albumsViewController)
+        albumsViewController.view.backgroundColor = .systemTeal
+        albumsViewController.tabBarItem = UITabBarItem(title: "Альбомы",
+                                                       image: UIImage(systemName: "rectangle.stack.fill"),
+                                                       tag: 2)
+
+        // MARK: - "Search" tab
+        let searchViewController = SearchViewController()
+        let searchNavigationController = UINavigationController(rootViewController: searchViewController)
+        searchViewController.tabBarItem = UITabBarItem(title: SearchViewController.Strings.navigationTitle,
+                                                       image: UIImage(systemName: "magnifyingglass"),
+                                                       tag: 3)
+
+        // MARK: - tabBarController settings
+        tabBarController.setViewControllers([
+            mediaLibruaryNavigationController,
+            forYouNavigationController,
+            albumsNavigationController,
+            searchNavigationController
+        ], animated: true)
+
+        tabBarController.selectedIndex = 2
+        tabBarController.tabBar.barStyle = .black
+
+        // MARK: - window settings
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
---
+++ IOS4_HW13_MIKHAIL_BAGMET/Sources/ForYou/Controllers/ForYouViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ForYouViewController.swift
+//  IOS4_HW13_MIKHAIL_BAGMET
+//
+//  Created by Михаил on 28.12.2021.
+//
+
+import UIKit
+
+class ForYouViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        setupNavigation()
+    }
+
+    // MARK: - Initializers
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    // MARK: - Settings
+
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+
+    // MARK: - Private functions
+
+    private func setupNavigation() {
+        navigationItem.title = Strings.navigationTitle
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+// MARK: - Constants
+
+extension ForYouViewController {
+    enum Strings {
+        static let navigationTitle = "Для Вас"
+    }
+}
